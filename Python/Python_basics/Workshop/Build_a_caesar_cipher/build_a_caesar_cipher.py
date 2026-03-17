0de945ddc11c4bd221d26fa1f60b24a048aefee6
Anarchist-Society <220625772+Anarchist-Society@users.noreply.github.com>
2026-03-17 23:10:17 +0100

    update python python basics workshop "Build a caesar cipher"

--- Python/Python_basics/Workshop/Build_a_caesar_cipher/build_a_caesar_cipher.py
+++ Python/Python_basics/Workshop/Build_a_caesar_cipher/build_a_caesar_cipher.py
@@ -1,8 +1,42 @@
-def caesar(text, shift):
-    alphabet = 'abcdefghijklmnopqrstuvwxyz'
-    shifted_alphabet = alphabet[shift:] + alphabet[:shift]
-    translation_table = str.maketrans(alphabet, shifted_alphabet)
-    return text.translate(translation_table)
+# Función para cifrar o descifrar un texto con el cifrado caesar
+def caesar(text, shift, encrypt=True):
 
-encrypted_text = caesar('freeCodeCamp', 3)
+    # Si shift no es un número devuelve error
+    if not isinstance(shift, int):
+        return 'Shift must be an integer value.'
+
+    # Si shift es menor que 1 o mayor que 25 devuelve error
+    if shift < 1 or shift > 25:
+        return 'Shift must be an integer between 1 and 25.'
+
+    # Alfabeto normal
+    alphabet = 'abcdefghijklmnopqrstuvwxyz'
+
+    # Si encrypt=False convertimos en negativo el shift
+    if not encrypt:
+        shift = - shift
+
+    # Creamos el alfabeto desplazado según el shift
+    shifted_alphabet = alphabet[shift:] + alphabet[:shift]
+
+    # Creamos una tabla que relaciona cada posición/letra del alfabeto con el alfabeto desplazado
+    translation_table = str.maketrans(alphabet + alphabet.upper(), shifted_alphabet + shifted_alphabet.upper())
+
+    # Reemplazamos cada letra de "text" con el alfabeto desplazado
+    encrypted_text = text.translate(translation_table)
+
+    return encrypted_text
+
+# Función para encriptar con el cifrado caesar
+def encrypt(text, shift):
+    return caesar(text, shift)
+
+# Función para descifrar con el cifrado caesar
+def decrypt(text, shift):
+    return caesar(text, shift, encrypt=False)
+
+encrypted_text = encrypt('hola', 1)
 print(encrypted_text)
+
+decrypt_text = decrypt(encrypted_text, 1)
+print(decrypt_text)
