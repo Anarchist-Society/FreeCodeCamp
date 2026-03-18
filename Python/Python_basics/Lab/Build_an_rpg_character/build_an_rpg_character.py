21cc5a8499a0c405ead51151ec46a26718081ea4
Anarchist-Society <220625772+Anarchist-Society@users.noreply.github.com>
2026-03-18 19:13:08 +0100

    udpate python python basics lab "Build an RPG Character"

--- Python/Python_basics/Lab/Build_an_rpg_character/build_an_rpg_character.py
+++ Python/Python_basics/Lab/Build_an_rpg_character/build_an_rpg_character.py
@@ -3,27 +3,35 @@ empty_dot = '○'
 
 def create_character(name, strength, intelligence, charisma):
 
+    # Si name no es un str devuelve error
     if not isinstance(name, str):
         return 'The character name should be a string'
 
+    # Si name está vacío devuelve error
     if name == '':
         return 'The character should have a name'
 
+    # Si la longitud de name es mayor que 10 devuelve error
     if len(name) > 10:
         return 'The character name is too long'
 
+    # Si name contiene espacio devuelve error
     if ' ' in name:
         return 'The character name should not contain spaces'
 
+    # Si strength o intelligence o charisma no es un int devuelve error
     if not isinstance(strength, int) or not isinstance(intelligence, int) or not isinstance(charisma, int):
         return 'All stats should be integers'
 
+    # Si strength o intelligence o charisma es menor que 1 devuelve error
     if strength < 1 or intelligence < 1 or charisma < 1:
         return 'All stats should be no less than 1'
 
+    # Si strength o intelligence o charisma es mayor que 4 devuelve error
     if strength > 4 or intelligence > 4 or charisma > 4:
         return 'All stats should be no more than 4'
 
+    # Si la suma de strength e intelligence y charisma no es igual a 7 devuelve error
     if strength + intelligence + charisma != 7:
         return 'The character should start with 7 points'
 
