851fd39c52fb45938ce9b177f91bdfde6b279c49
Anarchist-Society <220625772+Anarchist-Society@users.noreply.github.com>
2026-03-30 19:11:33 +0200

    add python python basics example lambda functions

--- Python/Loops_and_sequences/Working_with_loops_and_sequences/example_lambda_functions.py
+++ Python/Loops_and_sequences/Working_with_loops_and_sequences/example_lambda_functions.py
@@ -1,1 +1,11 @@
 print('What Are Lambda Functions and How Do They Work?\n')
+
+print('Ejemplo de función básica:')
+def square(num):
+    return num ** 2
+print(f'4 al cuadrado es: {square(4)}') # 16
+
+print('\nEjemplo de función lambda:')
+numbers = [1, 2, 3, 4, 5]
+even_numbers = list(filter(lambda x: x % 2 == 0, numbers))
+print(f'Números pares de la lista {numbers} usando una función lambda: {even_numbers}')
