d43d8d5a2dee286ed58724082bba42f762042ef8
Anarchist-Society <220625772+Anarchist-Society@users.noreply.github.com>
2026-04-18 15:19:37 +0200

    add python classes and objects example of classes and objects

--- Python/Classes_and_objects/Examples/Classes_and_objects/example_how_do_classes_work_and_how_do_they_differ_from_objects.py
+++ Python/Classes_and_objects/Examples/Classes_and_objects/example_how_do_classes_work_and_how_do_they_differ_from_objects.py
@@ -1,3 +1,28 @@
 print('How Do Classes Work and How Do They Differ From Objects?\n')
 
+print('Example 1:')
 
+class ClassName:
+    def __init__(self, name, age) -> None:
+        self.name = name
+        self.age = age
+
+    def sample_method(self):
+        print(self.name.upper())
+
+print('Example 2')
+
+class Dog:
+    def __init__(self, name, age) -> None:
+        self.name = name
+        self.age = age
+
+    def bark(self):
+        print(f'{self.name.upper()} says woof woof!')
+
+dog_1 = Dog('Jack', 3)
+dog_2 = Dog('Thatcher', 5)
+
+# Call the bark method
+dog_1.bark() # JACK says woof woof! I'm 3 years old!
+dog_2.bark() # THATCHER says woof woof! I'm 5 years old!
