f3a7f3915d06e8c4046e5510e4a9561987206276
Anarchist-Society <220625772+Anarchist-Society@users.noreply.github.com>
2026-04-18 17:38:50 +0200

    update python classes and objects examples of methods and attributes

--- Python/Classes_and_objects/Examples/Classes_and_objects/example_of_methods_and_attributes.py
+++ Python/Classes_and_objects/Examples/Classes_and_objects/example_of_methods_and_attributes.py
@@ -1,30 +1,30 @@
 print('What Are Methods and Attributes, and How Do They Work?\n')
 
 print('Example 1:\n')
-class Dog:
+class Dog_01:
     species = 'French Bulldog' # Class attribute
     def __init__(self, name):
         self.name = name # Instance attribute
 
-print(Dog.species) # French Bulldog
+print(Dog_01.species) # French Bulldog
 
-dog1 = Dog('Jack')
+dog1 = Dog_01('Jack')
 print(dog1.name) # Jack
 print(dog1.species) # French Bulldog
 
-dog2 = Dog('Tom')
+dog2 = Dog_01('Tom')
 print(dog2.name) # Tom
 print(dog2.species) # French Bulldog
 
 print('Example 2:\n')
 
-class Car:
+class Car_01:
     def __init__(self, color, model) -> None:
         self.color = color
         self.model = model
 
-car_1 = Car("red", "Toyota Corolla")
-car_2 = Car("green", "Lamborghini Revuelto")
+car_1 = Car_01("red", "Toyota Corolla")
+car_2 = Car_01("green", "Lamborghini Revuelto")
 
 print(car_1.model) # Toyota Corolla
 print(car_2.model) # Lamborghini Revuelto
@@ -33,7 +33,7 @@ print(car_1.color) # red
 print(car_2.color) # green
 
 print('Example 3: \n')
-class Dog:
+class Dog_02:
     species = 'French Bulldog'
 
     def __init__(self, name) -> None:
@@ -42,8 +42,8 @@ class Dog:
     def bark(self):
         return f'{self.name} says woof woof!'
 
-jack = Dog('Jack')
-jill = Dog('Jill')
+jack = Dog_02('Jack')
+jill = Dog_02('Jill')
 
 print(jack.bark()) # Jack says woof woof!
 print(jill.bark()) # Jill says woof woof!
