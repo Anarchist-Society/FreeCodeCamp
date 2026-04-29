9eefea1bff29f5fa16151aa862d1f58050051b93
Anarchist-Society <220625772+Anarchist-Society@users.noreply.github.com>
2026-04-29 20:36:16 +0200

    udpate ptyhon classes and object workwhop "Build an Email Simulator"

--- Python/Classes_and_objects/Workshop/Build_an_email_simulator/build_an_email_simulator.py
+++ Python/Classes_and_objects/Workshop/Build_an_email_simulator/build_an_email_simulator.py
@@ -1,5 +1,4 @@
 print('Build an Email Simulator:\n')
-
 import datetime
 
 class Email:
@@ -88,6 +87,12 @@ def main():
     tory = User('Tory')
     ramy = User('Ramy')
 
+    tory.send_email(ramy, 'Hello', 'Hi Ramy, just saying hello!')
+    ramy.send_email(tory, 'Re: Hello', 'Hi Tory, hope you are fine.')
     
+    ramy.check_inbox()
+    ramy.read_email(1)
+    ramy.delete_email(1)
+    ramy.check_inbox()
 if __name__ == '__main__':
     main()
