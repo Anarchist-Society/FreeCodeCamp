6ecd22390963ea2a98c7c2759042d3e79e673251
Anarchist-Society <220625772+Anarchist-Society@users.noreply.github.com>
2026-05-02 23:39:12 +0200

    update strucuture

---
+++ Python/Classes_and_objects/Certification_project/Build_a_budget_app/build_a_budget_app.py
@@ -0,0 +1,10 @@
+class Category:
+    def __init__(self, name):
+        self.name = name
+        self.ledger = []
+
+    def deposit(amount, description):
+        pass
+
+def create_spend_chart(categories):
+    pass
