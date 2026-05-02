3a076d15ad9c762bb8fa48831d6830169fdcb986
Anarchist-Society <220625772+Anarchist-Society@users.noreply.github.com>
2026-05-02 23:48:23 +0200

    update python classes and object certification project "Build a Budget App"

--- Python/Classes_and_objects/Certification_project/Build_a_budget_app/build_a_budget_app.py
+++ Python/Classes_and_objects/Certification_project/Build_a_budget_app/build_a_budget_app.py
@@ -3,8 +3,19 @@ class Category:
         self.name = name
         self.ledger = []
 
-    def deposit(amount, description):
-        pass
+    def deposit(self, amount, description):
+        if not description:
+            description = ''
 
 def create_spend_chart(categories):
     pass
+
+def main():
+    categoria_01 = Category('Juegos')
+    categoria_02 = Category('Libros')
+
+    categoria_01.deposit(100, 'Brawl Star')
+    categoria_02.deposit(50, '')
+
+if __name__ == '__main__':
+    main()
