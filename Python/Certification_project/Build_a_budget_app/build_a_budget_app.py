8c1998a1bf2d9b5c2ffc3b896097fd98961ed61d
Anarchist-Society <220625772+Anarchist-Society@users.noreply.github.com>
2026-05-12 22:09:18 +0200

    update python certification project "Build a Budget App"

--- Python/Certification_project/Build_a_budget_app/build_a_budget_app.py
+++ Python/Certification_project/Build_a_budget_app/build_a_budget_app.py
@@ -5,21 +5,33 @@ class Category:
 
     def deposit(self, amount, description = ""):
         self.ledger.append({'amount': amount, 'description': description})
+        print(self.ledger)
 
     def withdraw(self, amount, description = ""):
         amount = amount - amount * 2
 
         self.ledger.append({'amount': amount, 'description': description})
 
+        ingresos = 0
+
+        retiros = 0
+
+        for i in self.ledger:
+            if i['amount'] > 0:
+                ingresos += i['amount']
+            else:
+                retiros += abs(i['amount'])
+
+        return ingresos - retiros
+
 def create_spend_chart(categories):
     pass
 
 def main():
-    lista = []
-
-    lista.append({'amount': 100, 'description': 'Brawl Star'})
-
-    print(lista[0]['amount'])
+    food = Category('food')
+    food.deposit(900, 'deposit')
+    valor = food.withdraw(45.67, 'milk, cereal, eggs, bacon, bread')
+    print(valor)
 
 if __name__ == '__main__':
     main()
